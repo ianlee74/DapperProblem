a2070a927523c86f25a8e2ea0e23df13ba16b46f
Ian Lee <ilee@eahcs.com>
2015-08-12 15:24:09 -0500

    Simplified and changed so that the same query is being called.

--- DapperProblem/Program.cs
+++ DapperProblem/Program.cs
@@ -15,51 +15,33 @@ namespace DapperProblem
         {
             var p = new Program();
             Console.WriteLine("*** Get correct results (Id != SurveyId) ***");
-            var clientSurvey = p.GetCorrectResultsAsync(1).Result;
+            var clientSurvey = p.GetResultsAsync("NoDdmConnection").Result;
             Console.WriteLine("Id = {0}", clientSurvey.Id);
             Console.WriteLine("SurveyId = {0}", clientSurvey.SurveyId);
 
             Console.WriteLine();
-            Console.WriteLine("*** Get Incorrect results (Id == SurveyId) ***");            
-            clientSurvey = p.GetIncorrectResultsAsync(1).Result;
+            Console.WriteLine("*** Get Incorrect results (Id == SurveyId) ***");
+            clientSurvey = p.GetResultsAsync("DdmConnection").Result;
             Console.WriteLine("Id = {0}", clientSurvey.Id);
             Console.WriteLine("SurveyId = {0}", clientSurvey.SurveyId);
 
             Console.ReadLine();
         }
 
-        public async Task<ClientSurvey> GetCorrectResultsAsync(int clientId)
+        public async Task<ClientSurvey> GetResultsAsync(string connectionStringName)
         {
             const string sql =
-                @"select *
-                  from dbo.ClientSurveys cs
-                  where cs.ClientId = @clientId
-                    and isnull(cs.EndDate, '9999-1-1') > getdate() 
-                    and isnull(cs.StartDate, '2000-1-1') < getdate()
-                    and exists (select * from dbo.Surveys s where s.Id = cs.SurveyId and s.IsActive = 1);";
-
-            using (var cnn = await OpenConnectionAsync("NoDdmConnection"))
-            {
-                var surveys = (await cnn.QueryAsync<ClientSurvey>(sql, new { ClientId = clientId })).ToList();
-                var clientSurvey = surveys.First();
-                return clientSurvey;
-            }
-        }
-
-        public async Task<ClientSurvey> GetIncorrectResultsAsync(int clientId)
-        {      
-            const string sql =
                 @"select cs.*
-                  from dbo.ClientSurveys cs
-                  join dbo.Surveys s on s.Id = cs.SurveyId
-                  where cs.ClientId = @clientId
-                    and isnull(cs.EndDate, '9999-1-1') > getdate() 
-                    and isnull(cs.StartDate, '2000-1-1') < getdate() 
-                    and s.IsActive = 1;";
+                from dbo.ClientSurveys cs
+                join dbo.Surveys s on s.Id = cs.SurveyId
+                where cs.ClientId = 1
+                and isnull(cs.EndDate, '9999-1-1') > getdate() 
+                and isnull(cs.StartDate, '2000-1-1') < getdate() 
+                and s.IsActive = 1;";
 
-            using (var cnn = await OpenConnectionAsync("DdmConnection"))
+            using (var cnn = await OpenConnectionAsync(connectionStringName))
             {
-                var surveys = (await cnn.QueryAsync<ClientSurvey>(sql, new { ClientId = clientId })).ToList();
+                var surveys = (await cnn.QueryAsync<ClientSurvey>(sql)).ToList();
                 var clientSurvey = surveys.First();
                 return clientSurvey;
             }
